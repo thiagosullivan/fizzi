603f336e524aff1b06c7c44170bba3eb270fd6a1
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-14 14:56:24 -0300

    feat: add scroll trigger animation from gsap

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -8,8 +8,9 @@ import { Button } from "@/components/Button";
 import { TextSplitter } from "./TextSplitter";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Props for `Hero`.
@@ -48,6 +49,42 @@ const Hero: FC<HeroProps> = ({ slice }) => {
         opacity: 0,
         y: 10,
         duration: 0.6,
+      });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTl
+      .fromTo(
+        "body",
+        {
+          backgroundColor: "#FDE047",
+        },
+        {
+          backgroundColor: "#D9F99D",
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 40,
+        opacity: 0,
       });
   });
 
