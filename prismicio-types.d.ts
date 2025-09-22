9436e30a1d172677dec31e08a28fadd465e59336
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-21 22:54:52 -0300

    feat: add BigText section

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -70,6 +70,7 @@ type ContentRelationshipFieldWithData<
 }[Exclude<TCustomType[number], string>["id"]];
 
 type PageDocumentDataSlicesSlice =
+  | BigTextSlice
   | AlternatingTextSlice
   | CarouselSlice
   | SkyDiveSlice
@@ -217,6 +218,36 @@ type AlternatingTextSliceVariation = AlternatingTextSliceDefault;
 export type AlternatingTextSlice = prismic.SharedSlice<
   "alternating_text",
   AlternatingTextSliceVariation
+>;
+
+/**
+ * Default variation for BigText Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slices
+ */
+export type BigTextSliceDefault = prismic.SharedSliceVariation<
+  "default",
+  Record<string, never>,
+  never
+>;
+
+/**
+ * Slice variation for *BigText*
+ */
+type BigTextSliceVariation = BigTextSliceDefault;
+
+/**
+ * BigText Shared Slice
+ *
+ * - **API ID**: `big_text`
+ * - **Description**: BigText
+ * - **Documentation**: https://prismic.io/docs/slices
+ */
+export type BigTextSlice = prismic.SharedSlice<
+  "big_text",
+  BigTextSliceVariation
 >;
 
 /**
@@ -481,6 +512,9 @@ declare module "@prismicio/client" {
       AlternatingTextSliceDefaultPrimary,
       AlternatingTextSliceVariation,
       AlternatingTextSliceDefault,
+      BigTextSlice,
+      BigTextSliceVariation,
+      BigTextSliceDefault,
       CarouselSlice,
       CarouselSliceDefaultPrimary,
       CarouselSliceVariation,
