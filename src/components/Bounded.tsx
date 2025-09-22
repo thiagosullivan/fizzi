9af86519a246e828c1d2aab91046a6928dfc1e58
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-22 00:37:37 -0300

    fix: correcting typs on Bounded component

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,17 +1,18 @@
 import clsx from "clsx";
+import React from "react";
 
-type BoundedProps = {
-  as?: React.ElementType;
+type BoundedProps<T extends React.ElementType> = {
+  as?: T; // Agora 'as' é um componente genérico
   className?: string;
-  children: React.ReactNode;
-};
+  children: React.ReactNode; // Aceita qualquer tipo de conteúdo dentro do Bounded
+} & React.ComponentPropsWithRef<T>; // Herda todas as props do componente passado no 'as'
 
-export const Bounded = ({
+export const Bounded = <T extends React.ElementType = "section">({
   as: Comp = "section",
   className,
   children,
   ...restProps
-}: BoundedProps) => {
+}: BoundedProps<T>) => {
   return (
     <Comp
       className={clsx("px-4 first:pt-10 md:px-6", className)}
